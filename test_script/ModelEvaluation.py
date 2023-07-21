8834f78d92b3804cf551cab09a500d81b3fad2dc
Mai Hong (ARM) <gdg21vsa@login01.hpc.private>
2023-07-21 14:17:03 +0100

    ‘Delete unnecessary import'

--- test_script/ModelEvaluation.py
+++ test_script/ModelEvaluation.py
@@ -1,25 +1,15 @@
 import numpy as np
 from cmocean import cm
-import cartopy as cp
-import cartopy.crs as ccrs
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import xarray as xr
 import sys
-sys.path.append('/gpfs/home/mep22dku/scratch/SOZONE')
 #list of models
 sys.path.append('/gpfs/home/mep22dku/scratch/SOZONE/UTILS')
 import warnings
-from datetime import datetime
 warnings.filterwarnings('ignore')
-import cartopy.feature as cfeature
-from importlib import reload
-import matplotlib.path as mpath
 import glob
-import pickle
 import pandas as pd
-import seawater
-import time
 plt.rcParams.update({'font.size': 12})
 font = {'family' : 'monospace',
 'weight' : 'normal',
